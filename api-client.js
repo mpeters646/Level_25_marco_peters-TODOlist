a35b3f5e3f0061a37b80bc806d33e5933a41caf8
Marco Peters <m.peters646@gmail.com>
2021-06-25 12:19:21 +0200

    add api-client.js & modified index.html & script.js

---
+++ api-client.js
@@ -0,0 +1,56 @@
+const url = 'http://localhost:3000';
+
+const getDataFromAPI = async () => {
+  fetch(url, {
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        const list = document.querySelector('#todoList');
+        const listItem = document.createElement('li');
+        const icon = document.createElement('i');
+
+        list.appendChild(listItem);
+        listItem.innerHTML = data[i].description;
+        listItem.setAttribute('id', data[i]._id);
+        icon.className = 'far fa-trash-alt';
+        listItem
+          .insertBefore(icon, listItem.nextSibling)
+          .setAttribute('id', `${data[i]._id}`);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+getDataFromAPI();
+
+const postDataToAPI = async data => {
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  const json = response.json();
+  return json;
+};
+
+const addDataToDom = () => {
+  let valueInputField = document.querySelector('#inputField').value;
+  postDataToAPI({ description: valueInputField, done: false });
+  document.querySelector('#inputField').value = '';
+
+  window.location.reload();
+};
--- script.js
+++ script.js
@@ -1,60 +1,3 @@
-const url = 'http://localhost:3000';
-
-const getDataFromAPI = async () => {
-  fetch(url, {
-    headers: {
-      'Content-type': 'application/json',
-    },
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        const list = document.querySelector('#todoList');
-        const listItem = document.createElement('li');
-        const icon = document.createElement('i');
-
-        list.appendChild(listItem);
-        listItem.innerHTML = data[i].description;
-        listItem.setAttribute('id', data[i]._id);
-        icon.className = 'far fa-trash-alt';
-        listItem
-          .insertBefore(icon, listItem.nextSibling)
-          .setAttribute('id', `${data[i]._id}`);
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
-};
-
-// getDataFromAPI();
-
-const postDataToAPI = async data => {
-  const response = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-type': 'application/json',
-    },
-  });
-  const json = response.json();
-  return json;
-};
-
-const addDataToDom = () => {
-  let valueInputField = document.querySelector('#inputField').value;
-  postDataToAPI({ description: valueInputField, done: false });
-  document.querySelector('#inputField').value = '';
-
-  window.location.reload();
-};
-
 document.querySelector('#addTask').addEventListener('click', addDataToDom);
 document.querySelector('#inputField').addEventListener('keyup', event => {
   if (event.keyCode === 13) {
@@ -63,18 +6,17 @@ document.querySelector('#inputField').addEventListener('keyup', event => {
   }
 });
 
-/* testing */
-/* const listItem = document.getElementById(
-  'e2fefd6f-223d-4c14-8654-3c2fa34f5037'
-);
-console.log(listItem);
-listItem.addEventListener('click', () => {
-  alert('You clicked on a listitem');
-});
- */
-const iconItem = document.querySelector('i');
+const iconItem = document.querySelectorAll('i');
 console.log(iconItem);
 
-iconItem.addEventListener('click', () => {
-  alert('You clicked on a icon Item');
+// iconItem.addEventListener('click', () => alert('You clicked on a icon Item'));
+
+// iconItem.forEach(icon =>
+//   icon.addEventListener('click', () => alert('You clicked on a icon Item'))
+// );
+
+iconItem.forEach(icon => {
+  icon.addEventListener('click', () => {
+    alert('You clicked on a icon Item');
+  });
 });
